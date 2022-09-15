8dd678edd2571b2ff11b14ff46d29b223f70e22f
sami2004 <samuelkonderla@gmail.com>
2022-09-15 10:21:34 +0200

    Add project files.

---
+++ Kor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sugar, terulet, kerulet;
+
+            Console.Write("Kérem a kör sugarát: ");
+
+            string szoveg = Console.ReadLine();
+            sugar = Convert.ToDouble(szoveg);
+            
+
+            //r*r*PI, 2*r*PI//
+
+            terulet = Math.Pow(sugar, 2) * Math.PI;
+            kerulet = 2 * sugar * Math.PI;
+
+            Console.WriteLine($"Terület: {terulet}");
+            Console.WriteLine($"Kerület: {kerulet}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
